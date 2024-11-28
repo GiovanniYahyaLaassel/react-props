0a7257be276d11f46052fe312f6fc1969d235b8b
Laassel <lassel.giovanni@gmail.com>
2024-11-28 18:43:41 +0100

    Add Passing data as props

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,36 @@ import React, {useState} from "react";
 import Header from "./components/header";
 import BlogPost from "./components/BlogPost";
 import Footer from "./components/Footer";
-import { posts } from "./components/Data/posts (1)";
+import Card from "./components/Card";
+import  {posts} from "./components/Data/posts (1)";
+// filtro i post pubblicati
+const pubblishedPosts = posts.filter((post) => post.published);
+console.log('Pubblished posts:', pubblishedPosts);
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <div>
       <Header/>
-      <BlogPost/>
+      
+      {/* genero le card */}
+
+      {pubblishedPosts.map((post) => (
+        <Card 
+
+          key={post.id} 
+          title={post.title}
+          content={post.content}
+          image={post.image}
+          tags={post.tags} 
+        />
+      ))}
       <Footer/>
+
+    </div>
     </>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,14 +3,27 @@ import styles from "./Card.module.css";
 
 // Destrutturo i props e li faccio accettare da card
 function Card({title, content, image, tags}) {
+    console.log('title component:', title);
+    console.log('tags array:', tags);
+
 
     return (
         <article className={styles.card}>
             <div className={styles.imageContainer}>
+
                 {/* se un immagine e vuota oppire undefined allora uso quella di defualt */}
+
                 <img src={image ? image :"https://placehold.co/600x400"} alt={title} />
+            </div>
+            <h2>{title}</h2>
+            <p>{content}</p>
+            <div>
+ 
+       
             </div>
 
         </article>
     )
 }
+
+export default Card;
