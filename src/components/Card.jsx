5d2e348b80c65fe867f212ce834b01989ebde590
Laassel <lassel.giovanni@gmail.com>
2024-11-28 19:09:52 +0100

    ADD map method to create  span for every tags.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -16,12 +16,19 @@ function Card({title, content, image, tags}) {
                 <img src={image ? image :"https://placehold.co/600x400"} alt={title} />
             </div>
             <h2>{title}</h2>
-            <p>{content}</p>
-            <div>
+            {/* mappo per creare un span per ognuno */}
+            <div className={styles.tagsContainer}>
+                {tags.map((tag, index) => (
+                    <span key={index} className={styles.tag}>
+                        {tag}
+                    </span>
+                ))}
+
  
        
             </div>
 
+            <p>{content}</p>
         </article>
     )
 }
